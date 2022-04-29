e66472c799e5559b0a422b122cd06d7d29dfe079
nukit18 <vip.nukit@mail.ru>
2022-04-29 23:41:50 +0500

    task-arrays

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,39 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+    return data.split(',').sort(compare).join();
+}
+
+function compare(x, y) {
+    return x - y;
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+    let newData = [];
+    for (let e of data) {
+        if (e <= 100) {
+            newData.push(e);
+        }
+    }
+    return newData;
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    let result = [];
+    for (let i = 0; i < 5; i++) {
+        result.push(array1[i]);
+        result.push(array2[i]);
+    }
+    return result;
+}
